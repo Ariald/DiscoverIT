c8353144c390f6fe978656aca0cc368c998d7e53
DIEGO <DIEGOQC1410@GMAIL.COM>
2012-07-21 11:07:48 -0500

    final clase

--- DiscoverIT/Mantenimientos/RegistroVehiculos.aspx.vb
+++ DiscoverIT/Mantenimientos/RegistroVehiculos.aspx.vb
@@ -1,12 +1,25 @@
 ﻿Imports BusinessEntity
 Imports BusinessLogic
 Imports System.IO
+Imports DiscoverIT.sunarp
+
 Public Class RegistroVehiculos
     Inherits System.Web.UI.Page
 
+    Structure Vehiculo
+        Dim afabrica As String
+        Dim chazis As String
+        Dim codigo As String
+        Dim color As String
+        Dim marca As String
+        Dim modelo As String
+        Dim motor As String
+    End Structure
+
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not Page.IsPostBack Then
-           
+
         End If
     End Sub
 
@@ -34,10 +47,20 @@ Public Class RegistroVehiculos
     End Sub
 
     Protected Sub btnBuscar_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnBuscar.Click
-        lblMarca.Text = "Hiunday"
-        lblModelo.Text = "Tucson"
+
+        Dim objSunarp As New sunarp.VehiculoServiceImplService
+        Dim objVehiculo As Object
+        objVehiculo = objSunarp.consultarVehiculo(txtNroPlaca.Text)
+
+
+        'Dim sVehiculo As Vehiculo = DirectCast(objVehiculo, Vehiculo)
+
+        lblMarca.Text = DirectCast(objVehiculo, DiscoverIT.sunarp.vehiculo).marca
+        lblModelo.Text = DirectCast(objVehiculo, DiscoverIT.sunarp.vehiculo).modelo
         lblPropietario.Text = "Diego Quispe"
         lblSede.Text = "Lima"
+        txtMotor.Text = DirectCast(objVehiculo, DiscoverIT.sunarp.vehiculo).motor
+        txtDescripcion.Text = DirectCast(objVehiculo, DiscoverIT.sunarp.vehiculo).color + " - " + DirectCast(objVehiculo, DiscoverIT.sunarp.vehiculo).chazis
     End Sub
 
     Protected Sub btnCargar_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnCargar.Click
