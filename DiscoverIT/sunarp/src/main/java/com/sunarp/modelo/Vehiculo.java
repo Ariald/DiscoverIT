f8bac6b255cf4b54e1f4025039f84e8ec943bc25
DIEGO <DIEGOQC1410@GMAIL.COM>
2012-07-21 11:10:39 -0500

    sunarp ws

---
+++ DiscoverIT/sunarp/src/main/java/com/sunarp/modelo/Vehiculo.java
@@ -0,0 +1,152 @@
+package com.sunarp.modelo;
+
+public class Vehiculo {
+	/*
+	private String nombre;
+
+	private String apellidoPaterno;
+
+	private String apellidoMaterno;
+
+	private String direccion;
+
+	private String telefono;
+
+	private String documentoIdentidad;
+
+	private String tipoPersona;
+	
+	*/
+	
+	private String codigo;
+
+
+	private String marca;
+	private String modelo;
+	private String afabrica;
+	private String color;
+	private String motor;
+	private String chazis;
+	
+	// placa, marca, modelo, color,motor,nrochazis, año fabricacion
+	
+	
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getAfabrica() {
+		return afabrica;
+	}
+
+	public void setAfabrica(String afabrica) {
+		this.afabrica = afabrica;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getMotor() {
+		return motor;
+	}
+
+	public void setMotor(String motor) {
+		this.motor = motor;
+	}
+
+	public String getChazis() {
+		return chazis;
+	}
+
+	public void setChazis(String chazis) {
+		this.chazis = chazis;
+	}
+	
+/*
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDocumentoIdentidad() {
+		return documentoIdentidad;
+	}
+
+	public void setDocumentoIdentidad(String documentoIdentidad) {
+		this.documentoIdentidad = documentoIdentidad;
+	}
+
+	public String getTipoPersona() {
+		return tipoPersona;
+	}
+
+	public void setTipoPersona(String tipoPersona) {
+		this.tipoPersona = tipoPersona;
+	}
+
+*/	
+	
+	
+	
+}
